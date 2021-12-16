77cf27a17a3e45c4208dccaf18d6846ae222b7bf
Tessa Vu <vutessa.tv@gmail.com>
2021-12-15 20:25:02 -0700

    Update Experiments.java

--- lab02/Experiments.java
+++ lab02/Experiments.java
@@ -17,7 +17,6 @@ public class Experiments
 	{
 		// You will get input from the user, so you'll need a Scanner object.  
 		// Use this one, stored in 'in'.  
-		
 		Scanner in = new Scanner(System.in);
 		
 		/*****************
@@ -25,19 +24,15 @@ public class Experiments
 		 *****************/		
 		// Create a random 'word' using random numbers. This will represent
 		// a choice made by the computer.
-		
 		// Generate a random value, either 0, 1, or 2.
-		
 		int value = (int)(Math.random() * 3);
 		
 		/** Your work here -- fix the code so that it prints a 0, 1, or 2 randomly. **/
-		
 		System.out.println("Experiment #1: A random value: " + value);
 		
 		// This code converts the random value to a word. When it's working,
 		// the computer word should be either 'rock', 'paper', or 'scissors' with 
 		// equal probability. (Run it several times.)
-		
 		String computerWord = "No choice";
 		
 		if (value == 0)
@@ -53,32 +48,26 @@ public class Experiments
 		 * Experiment #2 *
 		 *****************/
 		// Use a loop to get input, wait for valid input */
-		
 		// Create a Boolean 'flag' to hold true or false. It will represent
 		// the idea that the user has supplied valid input. At the start
 		// of the program run, it's false. (No valid user input yet.)
-		
 		boolean userInputIsOK = false; 
 		
 		// We need a variable to hold the user input. For this experiment,
 		// we'll have the user input a string. We should initialize the variable
 		// with something, but since the user hasn't typed anything in yet,
 		// we need a 'nothing' string.
-		
 		String userWord = "";  // It may be better to use a null object. We'll cover it soon.
 		
 		// Our input loop.
-		
 		while (!userInputIsOK) // Loop as long as the user input is NOT ok.
 		{
 		    // Get input from the user.
-			
 			System.out.print("Experiment #2: Type a word and press enter: ");
 			userWord = in.next();
 
 		    // Test the input for validity.
 		    // If valid, set userInputIsOK to true.
-			
 			if (userWord.equals("rock")) // Remove this after testing.
 				userInputIsOK = true;
 			if (userWord.equals("paper"))
@@ -91,17 +80,13 @@ public class Experiments
 		}
 		
 		// Done.
-		
 		System.out.println("Experiment #2: You entered: " + userWord);
 
-		
 		/*****************
 		 * Experiment #3 *
 		 *****************/
 		// Compare two strings to see if they are equal.
-		
 		/** This code is incorrect. It seems to partially work, but doesn't. Your work will be to fix it. **/
-		
 	    if (userWord.equals(computerWord))
 	        System.out.println ("Experiment #3:  The words '" + computerWord + "' and '" + userWord + "' are equal.");
 	    else
