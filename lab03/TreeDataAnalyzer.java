c0357af637aa2efb03328631884129fabac4f44c
Tessa Vu <vutessa.tv@gmail.com>
2021-12-15 20:26:38 -0700

    Update TreeDataAnalyzer.java

--- lab03/TreeDataAnalyzer.java
+++ lab03/TreeDataAnalyzer.java
@@ -4,7 +4,6 @@
  * with a statement declaring the tree type with
  * the most trees.
  * @author		Tessa Vu
- * @professor	Dr. Jensen
  * @course		CS 1420-001
  * @version		07 September 2021
 */
@@ -17,7 +16,6 @@ import java.util.Scanner;
 
 public class TreeDataAnalyzer
 {
-
 	public static void main(String[] args) throws IOException
 	{
 		File filename = new File("forest.txt"); // A file object just represents a path to a file
@@ -36,55 +34,42 @@ public class TreeDataAnalyzer
 
 		file.close();
 		
-		// Reopen the file.  (Build a new Scanner, store it in our file
-        // variable.  The new Scanner scans from the beginning of the file.)
-        
+	// Reopen the file.  (Build a new Scanner, store it in our file
+        // variable.  The new Scanner scans from the beginning of the file.
         file = new Scanner(filename);
         
         // Create a few variables for keeping track of the 'best' tree
         // found so far.
-        
         String bestTreeName = null;
         int bestTreeCount = 0;
         
         // Loop, read in values, attempt to find the tree with the 
         // highest count.
-        
         int loopCount = 0;
         while (file.hasNext())
         {
             // Advance the counter.
-            
             loopCount++;
             
             // Read in the data
-            
             treeName = file.next(); // Use the scanner, scan the 'next' word from the file.
             treeCount = file.nextInt(); // Use the scanner, scan the 'next' int from the file. 
             
             // If it's the first tree, or if the current tree count
             // is higher than the best one found so far, keep it.
-            
             if (loopCount == 1 || treeCount > bestTreeCount)
             {
                 // We've just read in the best tree so far.  Keep a copy
                 // of it's data.
-                
                 bestTreeName = treeName; 
                 bestTreeCount = treeCount;
             }
-            
             // Loop end.
         }
-        
         // Print out the tree with the highest count.
-        
         System.out.println("'Best' tree: " + bestTreeName + " has a count of " + bestTreeCount + ".");        
 
         // Close the file.
-        
         file.close();
-
 	}
-
 }
