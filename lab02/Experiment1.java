4637d4ffad93e84ed65b03399191c6b1573f1b82
Tessa Vu <vutessa.tv@gmail.com>
2021-12-15 20:23:56 -0700

    Update Experiment1.java

--- lab02/Experiment1.java
+++ lab02/Experiment1.java
@@ -9,19 +9,15 @@ public class Experiment1
 		 *****************/		
 		// Create a random 'word' using random numbers. This will represent
 		// a choice made by the computer.
-		
 		// Generate a random value, either 0, 1, or 2.
-		
 		int value = (int)(Math.random() * 3);
 		
 		/** Your work here -- fix the code so that it prints a 0, 1, or 2 randomly. **/
-		
 		System.out.println("Experiment #1: A random value: " + value);
 		
 		// This code converts the random value to a word. When it's working,
 		// the computer word should be either 'rock', 'paper', or 'scissors' with 
 		// equal probability. (Run it several times.)
-		
 		String computerWord = "No choice";
 		
 		if (value == 0)
