04a40e81dda802eecdd35438c44826bb612e084c
Tessa Vu <vutessa.tv@gmail.com>
2021-12-15 20:24:13 -0700

    Update Experiment3.java

--- lab02/Experiment3.java
+++ lab02/Experiment3.java
@@ -6,21 +6,16 @@ public class Experiment3
 {
 	public static void main(String[] args)
 	{
-		
 		/*****************
 		 * Experiment #3 *
 		 *****************/
 		// Compare two strings to see if they are equal.
-		
 		/** This code is incorrect. It seems to partially work, but doesn't. Your work will be to fix it. **/
-		
 		Scanner input = new Scanner(System.in);
 		
 	    if (userWord.equals(computerWord))
 	        System.out.println ("Experiment #3: The words '" + computerWord + "' and '" + userWord + "' are equal.");
 	    else
-	        System.out.println ("Experiment #3: The words '" + computerWord + "' and '" + userWord + "' are not equal.");
-	    
+	        System.out.println ("Experiment #3: The words '" + computerWord + "' and '" + userWord + "' are not equal.");   
 	}
-
 }
